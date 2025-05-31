83db9e24ce0c5ff20652b0ec1db464de0ce3a3b9
Raul <ivanovraul555@gmail.com>
2025-05-31 14:30:31 +0300

    ansible для бд готов

---
+++ ansible/master_configs/dump.sql
@@ -0,0 +1,296 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+--CREATE ROLE postgres;
+--ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:5P+C0lxpQgfpHv3UKv3MlA==$KTnRwMpTCbEYW9K4RNwSz0jUFGC8y2joBo6WumX26TU=:p6I6o58l4foNxItaiHbpXO8EZyRp6CTP0ykizrO6uT8=';
+--CREATE ROLE repl_user;
+--ALTER ROLE repl_user WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN REPLICATION NOBYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:xUnviUq/+/8y8VDO8qpwEA==$4pfuem7AVX2wKAm17u2ksNKdowOBDY6DAPyr6fzYgs0=:Kd+IYLhLgwMmR8LG5crUinYSCCTbPR1R7WztuY3x0Ao=';
+
+--
+-- User Configurations
+--
+
+
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.13 (Debian 15.13-0+deb12u1)
+-- Dumped by pg_dump version 15.13 (Debian 15.13-0+deb12u1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "contacts" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.13 (Debian 15.13-0+deb12u1)
+-- Dumped by pg_dump version 15.13 (Debian 15.13-0+deb12u1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: contacts; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE contacts WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'ru_RU.UTF-8';
+
+
+ALTER DATABASE contacts OWNER TO postgres;
+
+\connect contacts
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: emails_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.emails_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.emails_id_seq OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: emails; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.emails (
+    id integer DEFAULT nextval('public.emails_id_seq'::regclass) NOT NULL,
+    email character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.emails OWNER TO postgres;
+
+--
+-- Name: phone_numbers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.phone_numbers_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.phone_numbers_id_seq OWNER TO postgres;
+
+--
+-- Name: phone_numbers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.phone_numbers (
+    id integer DEFAULT nextval('public.phone_numbers_id_seq'::regclass) NOT NULL,
+    phone_number character varying(12) NOT NULL
+);
+
+
+ALTER TABLE public.phone_numbers OWNER TO postgres;
+
+--
+-- Data for Name: emails; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.emails (id, email) FROM stdin;
+1	user1@gmail.com
+2	user2@gmail.com
+3	user3@gmail.com
+4	user4@gmail.com
+5	user5@gmail.com
+6	user6@gmail.com
+7	user7@gmail.com
+8	user8@gmail.com
+9	user9@gmail.com
+10	user10@gmail.com
+11	user11@gmail.com
+12	user12@gmail.com
+13	user13@gmail.com
+14	user14@gmail.com
+15	user15@gmail.com
+16	test1@gmail.com
+17	test2@gmail.com
+\.
+
+
+--
+-- Data for Name: phone_numbers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.phone_numbers (id, phone_number) FROM stdin;
+1	+79123456781
+2	+79123456782
+3	+79123456783
+4	+79123456784
+5	+79123456785
+6	+79123456786
+7	+79123456787
+8	+79123456788
+9	+79123456789
+10	+79123456790
+11	+79123456791
+12	+79123456792
+13	+79123456793
+14	+79123456794
+15	+79123456795
+16	89998765432
+17	+79998765432
+18	+79036665456
+19	+79998908877
+20	+79036665451
+21	+79998908871
+\.
+
+
+--
+-- Name: emails_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.emails_id_seq', 17, true);
+
+
+--
+-- Name: phone_numbers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.phone_numbers_id_seq', 21, true);
+
+
+--
+-- Name: emails emails_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.emails
+    ADD CONSTRAINT emails_email_key UNIQUE (email);
+
+
+--
+-- Name: emails emails_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.emails
+    ADD CONSTRAINT emails_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: phone_numbers phone_numbers_phone_number_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.phone_numbers
+    ADD CONSTRAINT phone_numbers_phone_number_key UNIQUE (phone_number);
+
+
+--
+-- Name: phone_numbers phone_numbers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.phone_numbers
+    ADD CONSTRAINT phone_numbers_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "postgres" dump
+--
+
+\connect postgres
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.13 (Debian 15.13-0+deb12u1)
+-- Dumped by pg_dump version 15.13 (Debian 15.13-0+deb12u1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- PostgreSQL database cluster dump complete
+--
